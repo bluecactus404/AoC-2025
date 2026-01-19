0192378e74e1ec54d05bebe25eecb853cc7a9098
bluecactus404 <127633281+bluecactus404@users.noreply.github.com>
2026-01-18 23:26:54 -0800

    Solved part 1

---
+++ d5/d5.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+
+
+def parse_input(file_name:str):
+    PATH = Path(file_name)
+    with open(PATH, 'r') as f:
+        lines = f.readlines()
+        
+
+
+
+
+
+def run(file_name:str):
